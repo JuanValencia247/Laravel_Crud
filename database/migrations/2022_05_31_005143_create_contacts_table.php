998a7658e8041af3c98ccd00ab623420970b7ab0
JuanValencia247 <aneuidi@gmail.com>
2022-05-31 09:02:58 -0500

    Crud laravel finalizado

---
+++ database/migrations/2022_05_31_005143_create_contacts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->tinyInteger('age', unsigned: true);
+            $table->timestamps();
+            // crear una clave foreingkiey a la tabla user
+            //$table->foreignIdFor(User::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
